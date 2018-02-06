f309892fd722bb6471e1f9173b3aa5b1021a8c20
potato <hksahenry@yahoo.com.hk>
2018-02-06 13:47:24 +0800

    renew

---
+++ cgi-bin/helloworld.py
@@ -0,0 +1,21 @@
+import cgi
+import cgitb
+import time
+import os
+
+header = "Content-type: text/html\n\n"
+
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello World</title>
+</head>
+<body>
+  <h2>Hello World!</h2>
+  <p>Finally can see you!</p>
+</body>
+</html>
+
+print header + html
