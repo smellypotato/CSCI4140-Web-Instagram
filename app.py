766b669ff74befc01b9a931de5878588c0e83edf
potato <hksahenry@yahoo.com.hk>
2018-02-06 14:48:50 +0800

    ch

--- app.py
+++ app.py
@@ -9,21 +9,11 @@ import CGIHTTPServer
 import webbrowser
 
 PORT = 8080
-#TODO: check that port is available,
-# and look for a different one if it isn't.
-
-script_path = "cgi-bin/helloworld.py"
 
 server_class = BaseHTTPServer.HTTPServer
 handler_class = CGIHTTPServer.CGIHTTPRequestHandler
 server_address = ("", PORT)
 
 httpd = server_class(server_address, handler_class)
-
-url = 'http://localhost:{0}/{1}'.format(PORT, script_path)
-
-webbrowser.open_new_tab(url)
-
-print "serving at", url
 
 httpd.serve_forever()
