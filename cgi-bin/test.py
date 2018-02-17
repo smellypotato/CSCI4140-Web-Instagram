35d4457a236decf0ab0e7039a0b312bd10e4b8bd
potato <hksahenry@yahoo.com.hk>
2018-02-17 19:40:30 +0800

    test

---
+++ cgi-bin/test.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+import cgi
+import cgitb
+
+url = "/cgi-bin/index.py"
+print 'Content-type:text/html'
+print cookie.output()
+print
+print '<html>'
+print '<head>'
+print '<title>Web Instagram</title>'
+print '</head>'
+print '<body>'
+print '<p>Redirected......hi!</p>'
+print '</body>'
+print '</html>'
+
+#executable?
