3c2246e31458f7adaee702ea18a5b80c378d7dbd
potato <hksahenry@yahoo.com.hk>
2018-02-17 23:52:36 +0800

    wdqwd

--- app.py
+++ app.py
@@ -11,12 +11,15 @@ import webbrowser
 PORT = 8080
 #TODO: check that port is available,
 # and look for a different one if it isn't.
+#script_path = "cgi-bin/redirect.py"
 
 server_class = BaseHTTPServer.HTTPServer
 handler_class = CGIHTTPServer.CGIHTTPRequestHandler
 server_address = ("", PORT)
 CGIHTTPServer.CGIHTTPRequestHandler.cgi_directories = ["/cgi-bin"]
 
+#url = 'http://localhost:{0}/{1}'.format(PORT, script_path)
+#webbrowser.open_new_tab(url)
 httpd = server_class(server_address, handler_class)
 
 httpd.serve_forever()
--- cgi-bin/redirect.py
+++ cgi-bin/redirect.py
@@ -13,14 +13,14 @@ cookie['session'] = ''
 cookie['session']['expires'] = 'Thu, 01 Jan 1970 00:00:00 PST'
 
 #url = 'http://localhost:{0}/{1}'.format(8080, "cgi-bin/index.py")
-#url = "/cgi-bin/test.py"
+url = "/cgi-bin/index.py"
 print 'Content-type:text/html'
 print cookie.output()
 print
 print '<html>'
 print '<head>'
 print '<title>Web Instagram</title>'
-#print '<META HTTP-EQUIV="Refresh" CONTENT=0;URL="%s">'%url
+print '<META HTTP-EQUIV="Refresh" CONTENT=0;URL="%s">'%url
 print '</head>'
 print '<body>'
 print '<p>Redirecting......hi!</p>'
