e994651c764094fe7f701d0f90c4a1822662ee4c
potato <hksahenry@yahoo.com.hk>
2018-02-17 16:24:45 +0800

    test

---
+++ cgi-bin/test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+print "<!DOCTYPE>"
+print ""
+print '<html>'
+print '<head>'
+print '<title>Web Instagram</title>'
+print '</head>'
+print '<body>'
+print 'hello'
+print '</body>'
+print '</html>'
