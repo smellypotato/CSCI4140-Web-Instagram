e9dfd0c382be54183ba47bb3c33040d4a80d0f7d
potato <hksahenry@yahoo.com.hk>
2018-02-18 20:49:07 +0800

    ?

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 
-import cgi
+'''import cgi
 import cgitb
 import time
 import os
 import sqlite3
 import Cookie
 import math
-'''cookie = ""
+cookie = ""
 cgitb.enable()
 login = False
 user = "guest"
