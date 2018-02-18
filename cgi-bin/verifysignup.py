c8043aeb0073b84312013cb0cb83784db13a2283
potato <hksahenry@yahoo.com.hk>
2018-02-19 03:18:54 +0800

    3

--- cgi-bin/verifysignup.py
+++ cgi-bin/verifysignup.py
@@ -50,7 +50,7 @@ print '<title>Web Instagram</title>'
 print '<META HTTP-EQUIV="Refresh" CONTENT="1;URL=%s">'%url
 print '</head>'
 print '<body>'
-print '<p>26</p>'
+print '<p>3</p>'
 print '<p>%s</p>'%cookie.output()
 print fail
 
