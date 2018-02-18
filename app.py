18d3989efec6db1f88cbcb7dbe03081b2aa117a3
potato <hksahenry@yahoo.com.hk>
2018-02-18 20:34:16 +0800

    html?

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import webbrowser
 PORT = 8080
 #TODO: check that port is available,
 # and look for a different one if it isn't.
-script_path = "cgi-bin/redirect.py"
+#script_path = "cgi-bin/redirect.py"
 
 server_class = BaseHTTPServer.HTTPServer
 handler_class = CGIHTTPServer.CGIHTTPRequestHandler
@@ -19,8 +19,8 @@ server_address = ("", PORT)
 handler_class.cgi_directories = ["/cgi-bin"]
 
 #url = 'http://localhost:{0}/{1}'.format(PORT, script_path)
-url = script_path
-webbrowser.open_new_tab(url)
+#url = script_path
+#webbrowser.open_new_tab(url)
 httpd = server_class(server_address, handler_class)
 
 httpd.serve_forever()
