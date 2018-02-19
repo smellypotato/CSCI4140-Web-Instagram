87ed410b7f451f49c559140442789533a5810187
potato <hksahenry@yahoo.com.hk>
2018-02-19 16:58:59 +0800

    11

--- cgi-bin/filter.py
+++ cgi-bin/filter.py
@@ -23,8 +23,39 @@ if (os.path.isfile(output)):
         os.remove(img)
     os.rename(output, img)
 cmd=""
+thisimg = os.path.join('..',img)
 #image = pm.Image(img)
-
+if (filt == "border"):
+    cmd = 'convert '+ thisimg + ' -bordercolor Black -border 100x100 '+ output
+    subprocess.Popen(cmd)
+    #image.borderColor("Black")
+    #geo =str(int(image.size().width()*0.1))+'x'+str(int(image.size().height()*0.1))
+    #image.border(geo)
+elif (filt == "lomo"):
+    cmd = 'convert ' + thisimg + ' -channel R -level 33% -channel -G -level 33% ' + output
+    subprocess.Popen(cmd)
+    #cmd = 'convert ..\\'+ img + ' -channel R -level 33% -channel G -level 33% ..\\'+ output
+elif (filt == "lensflare"):
+    cmd = 'convert lensflare.png -resize 200x200 tmp.png'
+    subprocess.Popen(cmd)
+    cmd = 'composite -compose screen -gravity northwest tmp.png ' + thisimg + ' ' +output
+    subprocess.Popen(cmd)
+    #flare = pm.Image("lensflare.png")
+    #geo =str(image.size().width())+'x'+str(image.size().height())+'!'
+    #flare.resize(geo)#+str(image.size().height()))
+    #image.composite(flare,pm.GravityType.NorthWestGravity,pm.CompositeOperator.ScreenCompositeOp)
+#elif (filt == "blackwhite"):
+    #cmd =
+    #image.colorSpace(pm.ColorspaceType.GRAYColorspace)
+    #if not image.monochrome():
+    #    image.monochrome(True)
+    #bw = pm.Image("bwgrad.png")
+    #geo =str(image.size().width())+'x'+str(image.size().height())+'!'
+    #bw.resize(geo)
+    #image.composite(bw,pm.GravityType.CenterGravity,pm.CompositeOperator.SoftLightCompositeOp)
+elif (filt == "blur"):
+    cmd = 'convert ' + thisimg + '-blur 0.5x2 ' + output
+    subprocess.Popen(cmd)
     #image.blur(5,5)
 #image.write(output)
 #url = 'http://localhost:{0}/{1}'.format(8080, "cgi-bin/editor.py")
@@ -37,8 +68,8 @@ print '<title>Web Instagram</title>'
 print '<META HTTP-EQUIV="Refresh" CONTENT="1;URL=%s">'%url
 print '</head>'
 print '<body>'
-print '<p>Applying filter......%s %s</p>'%img, output
-#print '<img src = %s>'%os.path.join('..',output)
+print '<p>Applying filter......%s %s</p>'%(img, output)
+print '<img src = %s>'%os.path.join('..',output)
 #print '<form action ="editor.py" method = "post">'
 #print '<input type = "hidden" value = "%s" name = "imgname">'%img
 #print '<input type = "hidden" value = "%s" name = "owner">'%owner
