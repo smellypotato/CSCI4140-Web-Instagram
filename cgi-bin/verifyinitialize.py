f75a1e753abe201cf5c2bcc1b33af8a57b3c68d1
potato <hksahenry@yahoo.com.hk>
2018-02-20 00:09:14 +0800

    ww

--- cgi-bin/verifyinitialize.py
+++ cgi-bin/verifyinitialize.py
@@ -1,38 +1,5 @@
 #! /usr/bin/env python
-import cgi
-import cgitb
-import time
-import os
-import sqlite3
-cgitb.enable()
-
-form = cgi.FieldStorage()
-uid = form.getvalue("username")
-pw = form.getvalue("password")
-newpw = form.getvalue("newpassword")
-
-login == False
-
-conn = sqlite3.connect('admin.db')
-cursor = conn.cursor()
-cursor.execute("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='admin';")
-if cursor.fetchone()[0] == 0 and uid == "admin" and pw == "admin":
-    login = True
-    cursor = conn.execute('''
-    CREATE TABLE admin(
-    name TEXT PRIMARY KEY,
-    password TEXT'''
-    )
-    cursor = conn.execute('INSERT INTO admin VALUES(?,?)',(uid,pw))
-    conn.commit()
-else:
-    cursor = conn.cursor()
-    cursor.execute("SELECT count(*) FROM admin WHERE username= ? AND password = ?", (uid,pw))
-    if cursor.fetchone()[0] == 1:
-        login = True
-        conn.execute("UPDATE account SET password = ? WHERE username= ? AND password = ?", (newpw, uid, pw))
-        conn.commit()
-
+login = False
 url = "/cgi-bin/index.py"
 print 'Content-type:text/html'
 print
